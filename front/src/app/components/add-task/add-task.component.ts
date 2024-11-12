b8449028939b6a97677b172eb39865c91a4692d6
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-12 10:40:23 -0300

    implementando add-task

--- front/src/app/components/add-task/add-task.component.ts
+++ front/src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+
+  tarefa: string = '';
+  categoria: string = '';
+  concluido: boolean = false;
+
+
+
+  onSubmit(){
+
+  }
 
 }
