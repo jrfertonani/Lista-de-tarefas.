6b1ff8bb4f15f5d6e5d3c58ba3e65146c14d66a5
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-11 21:37:08 -0300

    Tarefa.ts - module

--- front/src/app/services/tasks.service.ts
+++ front/src/app/services/tasks.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class TasksService {
 
-  constructor() { }
+  private URL = "http://localhost:3000/tasks";
+
+  constructor(private http: HttpClient) { }
+
+
+  getTasks(): {
+
+  }
+
+
 }
