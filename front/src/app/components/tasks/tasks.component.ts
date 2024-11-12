145910df6a8189f4cb53f2845be19e150cb26705
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-11 21:50:07 -0300

    getTask components

--- front/src/app/components/tasks/tasks.component.ts
+++ front/src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa } from '../../../Tarefa';
+import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit{
+
+  tarefas: Tarefa[] = [];
+
+
+  constructor(private taskService: TasksService ){}
+
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((dados) => {
+      this.tarefas = dados;
+      console.log(dados);
+    })
+
+  }
+
 
 }
